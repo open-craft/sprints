76fc8a2ba695bc0462ef37697ba99d468c1803e4
Agrendalath <piotr@surowiec.it>
2019-09-02 19:44:26 +0200

    [BB-1547] Make table more readable

--- frontend/src/components/UserTable.js
+++ frontend/src/components/UserTable.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './Table.css';
 import {PATH_JIRA_ISSUE} from "../constants";
 
-const nameColumn = {width: '20%'};  // 2 cells -> 40%
-const timeColumn = {width: '10%'};  // 6 cells -> 60% total
+const nameColumn = {width: '25%'};  // 1 cells -> 25% total
+const timeColumn = {width: '15%'};  // 5 cells -> 75% total
 
 const UserTable = ({list, username}) =>
     <table className="table">
@@ -12,20 +12,14 @@ const UserTable = ({list, username}) =>
             <td style={timeColumn}>
                 Key
             </td>
-            <td style={nameColumn}>
-                Assignee
-            </td>
-            <td style={nameColumn}>
-                Reviewer 1
-            </td>
             <td style={timeColumn}>
+                Working As
+            </td>
+            <td style={nameColumn}>
                 Status
             </td>
             <td style={timeColumn}>
-                Assignee Time
-            </td>
-            <td style={timeColumn}>
-                Reviewer Time
+                Remaining Time
             </td>
             <td style={timeColumn}>
                 Current Sprint
@@ -42,27 +36,21 @@ const UserTable = ({list, username}) =>
                     <a href={PATH_JIRA_ISSUE + item.key} title={item.summary} target="_blank"
                        rel="noopener noreferrer">{item.key}</a>
                 </td>
-                <td style={nameColumn}>
-                    {item.assignee}
-                </td>
-                <td style={nameColumn}>
-                    {item.reviewer_1}
-                </td>
                 <td style={timeColumn}>
+                    {
+                        item.assignee === username
+                            ? "Assignee"
+                            : "Reviewer"
+                    }
+                </td>
+                <td style={nameColumn}>
                     {item.status}
                 </td>
                 <td style={timeColumn}>
                     {
                         item.assignee === username
                             ? Math.round(item.assignee_time / 3600)
-                            : null
-                    }
-                </td>
-                <td style={timeColumn}>
-                    {
-                        item.reviewer_1 === username
-                            ? Math.round(item.review_time / 3600)
-                            : null
+                            : Math.round(item.review_time / 3600)
                     }
                 </td>
                 <td style={timeColumn}>

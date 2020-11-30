6c161a4554177b035115156fb71ce28c749fd317
Farhaan Bukhsh <farhaan@opencraft.com>
2020-12-01 01:06:29 +0530

    Fix tests
    Signed-off-by: Farhaan Bukhsh <farhaan@opencraft.com>

--- sprints/dashboard/libs/tests/test_mattermost.py
+++ sprints/dashboard/libs/tests/test_mattermost.py
@@ -22,7 +22,7 @@ def test_message_format(mock_mattermost):
     get_usernames_from_emails_mock.side_effect = side_effect_usernames_from_emails
     post_message_to_channel_mock = mock_mattermost.return_value.__enter__.return_value.post_message_to_channel
     create_mattermost_post('Hello, World!', ['farhaan@example.com', 'piotr@example.com', 'demid@example.com'])
-    post_message_to_channel_mock.assert_called_with('test_channel', '@farhaan, @piotr, @demid : Hello, World!')
+    post_message_to_channel_mock.assert_called_with('test_channel', '@farhaan, @piotr, @demid: Hello, World!')
 
 
 @override_settings(MATTERMOST_CHANNEL='test_channel')
@@ -32,7 +32,7 @@ def test_with_no_username(mock_mattermost):
     get_usernames_from_emails_mock.side_effect = side_effect_usernames_from_emails
     post_message_to_channel_mock = mock_mattermost.return_value.__enter__.return_value.post_message_to_channel
     create_mattermost_post('Hello, World!', ['nousername@example.com', 'piotr@example.com', 'demid@example.com'])
-    post_message_to_channel_mock.assert_called_with('test_channel', '@piotr, @demid : Hello, World!')
+    post_message_to_channel_mock.assert_called_with('test_channel', '@piotr, @demid: Hello, World!')
 
 
 @override_settings(MATTERMOST_CHANNEL='test_channel')
